2f8d7303d20ce238145f93d61d2a51e15d5ef22a
Jon Q <hello@jonquach.com>
2019-04-16 23:12:21 -0400

    Add actions api

--- docs/api/README.md
+++ docs/api/README.md
@@ -2,4 +2,5 @@
 
 ### Table of Contents
 
+- [actions](./actions.md)
 - [selectors](./selectors.md)
--- src/commands/action.commands.js
+++ src/commands/action.commands.js
@@ -1,15 +1,75 @@
 import fireEvent from '../fireEvent'
 import { typeCommand } from '../utils/keyEvent.utils'
 
+/**
+ * Checks a DOM element. Typically used for `<input />`.
+ *
+ * @returns {Cyan} The Cyan instance.
+ *
+ * @example
+ * cy.get('input[type=checkbox]').check()
+ */
 function check() {
   const node = this.__getNode('check')
   node.checked = true
   return this
 }
 
+/**
+ * Clear the value of a DOM element. Typically used for `<input />`.
+ *
+ * @returns {Cyan} The Cyan instance.
+ *
+ * @example
+ * cy.get('input[type=text]').clear()
+ */
 function clear() {
   const node = this.__getNode('clear')
   node.value = ''
+  return this
+}
+
+/**
+ * Triggers an Event for a DOM element.
+ *
+ * @returns {Cyan} The Cyan instance.
+ *
+ * @example
+ * cy.get('textarea').trigger('focus')
+ */
+function trigger(event) {
+  const node = this.__getNode('trigger')
+  fireEvent[event](node)
+  jest.runAllTimers()
+  return this
+}
+
+/**
+ * Types characters/commands into a DOM element. Typically used for `<input />`.
+ *
+ * @returns {Cyan} The Cyan instance.
+ *
+ * @example
+ * cy.get('input[type=text]').type('Hello')
+ */
+function type(value) {
+  const node = this.__getNode('type', value)
+  node.value = value
+  typeCommand(value)
+  return this
+}
+
+/**
+ * Unchecks a DOM element. Typically used for `<input />`.
+ *
+ * @returns {Cyan} The Cyan instance.
+ *
+ * @example
+ * cy.get('input[type=checkbox]').uncheck()
+ */
+function uncheck() {
+  const node = this.__getNode('uncheck')
+  node.checked = false
   return this
 }
 
@@ -20,25 +80,35 @@ function hover() {
   return this
 }
 
-function trigger(event) {
-  const node = this.__getNode('trigger')
-  fireEvent[event](node)
-  jest.runAllTimers()
-  return this
-}
-
-function type(value) {
-  const node = this.__getNode('type', value)
-  node.value = value
-  typeCommand(value)
-  return this
-}
-
-function uncheck() {
-  const node = this.__getNode('uncheck')
-  node.checked = false
-  return this
-}
+/**
+ * @name events
+ * @module Cyan
+ *
+ * @description Triggers an Event for a DOM element.
+ *
+ * Supported events include:
+ *
+ * ```
+ * 'copy', 'cut', 'paste', 'compositionEnd', 'compositionStart',
+ * 'compositionUpdate', 'keyDown', 'keyPress', 'keyUp', 'focus', 'blur',
+ * 'focusIn', 'focusOut', 'change', 'input', 'invalid', 'submit', 'click',
+ * 'contextMenu', 'dblClick', 'drag', 'dragEnd', 'dragEnter', 'dragExit',
+ * 'dragLeave', 'dragOver', 'dragStart', 'drop', 'mouseDown', 'mouseEnter',
+ * 'mouseLeave', 'mouseMove', 'mouseOut', 'mouseOver', 'mouseUp', 'select',
+ * 'touchCancel', 'touchEnd', 'touchMove', 'touchStart', 'scroll', 'wheel',
+ * 'abort', 'canPlay', 'canPlayThrough', 'durationChange', 'emptied',
+ * 'encrypted', 'ended', 'loadedData', 'loadedMetadata', 'loadStart', 'pause',
+ * 'play', 'playing', 'progress', 'rateChange', 'seeked', 'seeking', 'stalled',
+ * 'suspend', 'timeUpdate', 'volumeChange', 'waiting', 'load', 'error',
+ * 'animationStart', 'animationEnd', 'animationIteration', 'transitionEnd',
+ * 'doubleClick'
+ * ```
+ *
+ * @returns {Cyan} The Cyan instance.
+ *
+ * @example
+ * cy.get('textarea').hover()
+ */
 
 const commands = {
   check,
