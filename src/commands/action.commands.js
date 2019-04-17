ab87b8172c5c3239959ea133ceac25d604ffae32
Jon Q <hello@jonquach.com>
2019-04-16 23:16:06 -0400

    Update Actions doc [skip ci]

--- src/commands/action.commands.js
+++ src/commands/action.commands.js
@@ -91,17 +91,17 @@ function hover() {
  * ```
  * 'copy', 'cut', 'paste', 'compositionEnd', 'compositionStart',
  * 'compositionUpdate', 'keyDown', 'keyPress', 'keyUp', 'focus', 'blur',
- * 'focusIn', 'focusOut', 'change', 'input', 'invalid', 'submit', 'click',
- * 'contextMenu', 'dblClick', 'drag', 'dragEnd', 'dragEnter', 'dragExit',
- * 'dragLeave', 'dragOver', 'dragStart', 'drop', 'mouseDown', 'mouseEnter',
- * 'mouseLeave', 'mouseMove', 'mouseOut', 'mouseOver', 'mouseUp', 'select',
- * 'touchCancel', 'touchEnd', 'touchMove', 'touchStart', 'scroll', 'wheel',
- * 'abort', 'canPlay', 'canPlayThrough', 'durationChange', 'emptied',
- * 'encrypted', 'ended', 'loadedData', 'loadedMetadata', 'loadStart', 'pause',
- * 'play', 'playing', 'progress', 'rateChange', 'seeked', 'seeking', 'stalled',
- * 'suspend', 'timeUpdate', 'volumeChange', 'waiting', 'load', 'error',
- * 'animationStart', 'animationEnd', 'animationIteration', 'transitionEnd',
- * 'doubleClick'
+ * 'focusIn', 'focusOut', 'change', 'hover', 'input', 'invalid', 'submit',
+ * 'click', 'contextMenu', 'dblClick', 'drag', 'dragEnd', 'dragEnter',
+ * 'dragExit', 'dragLeave', 'dragOver', 'dragStart', 'drop', 'mouseDown',
+ * 'mouseEnter', 'mouseLeave', 'mouseMove', 'mouseOut', 'mouseOver',
+ * 'mouseUp', 'select', 'touchCancel', 'touchEnd', 'touchMove', 'touchStart',
+ * 'scroll', 'wheel', 'abort', 'canPlay', 'canPlayThrough', 'durationChange',
+ * 'emptied', 'encrypted', 'ended', 'loadedData', 'loadedMetadata',
+ * 'loadStart', 'pause', 'play', 'playing', 'progress', 'rateChange',
+ * 'seeked', 'seeking', 'stalled', 'suspend', 'timeUpdate', 'volumeChange',
+ * 'waiting', 'load', 'error', 'animationStart', 'animationEnd',
+ * 'animationIteration', 'transitionEnd', 'doubleClick'
  * ```
  *
  * @returns {Cyan} The Cyan instance.
