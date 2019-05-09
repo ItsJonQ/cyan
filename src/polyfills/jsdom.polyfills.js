e2141e16fde0ea4f701e6c73552217bae19cc179
Jon Quach <hello@jonquach.com>
2019-05-09 13:22:30 -0400

    setupTests: Add mock IntersectionObserver (#9)
    This update adds a mock implementation of IntersectionObserver.
    The IntersectionObserver methods just return null.

--- src/polyfills/jsdom.polyfills.js
+++ src/polyfills/jsdom.polyfills.js
@@ -1,12 +1,27 @@
 const setupJSDOM = () => {
   beforeEach(() => {
+    /**
+     * window
+     */
+    window.IntersectionObserver = () => ({
+      disconnect: () => null,
+      observe: () => null,
+      takeRecords: () => null,
+      unobserve: () => null,
+    })
+
     window.getSelection = () => {
       return {
         addRange: () => ({}),
         removeAllRanges: () => ({}),
       }
     }
+
     window.scrollTo = () => null
+
+    /**
+     * document
+     */
     document.createRange = () => ({
       setStart: () => {},
       setEnd: () => {},
@@ -21,8 +36,16 @@ const setupJSDOM = () => {
   })
 
   afterEach(() => {
+    /**
+     * window
+     */
+    window.IntersectionObserver = undefined
     window.getSelection = undefined
     window.scrollTo = undefined
+
+    /**
+     * document
+     */
     document.createRange = undefined
     document.execCommand = undefined
   })
